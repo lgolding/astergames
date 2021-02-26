faa233f125fd8e6f1945a662ed3287716df95309
Larry Golding <larrygolding@comcast.net>
2021-02-26 11:21:18 -0800

    Use async/await instead of promise on connection.

--- server/server.ts
+++ server/server.ts
@@ -2,21 +2,25 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { connectionString } from './config/keys';
 
-const app = express();
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 
-app.use(express.json());
+runServer(port);
 
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to database.'))
-  .catch(err => {
+async function runServer(port: number) {
+  const app = express();
+
+  app.use(express.json());
+
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to database.');
+  } catch (err) {
     console.log(`Failed to connect to database: ${err}`);
     process.exit(1);
-  });
+  }
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}.`));
+  app.listen(port, () => console.log(`Server started on port ${port}.`));
+}
