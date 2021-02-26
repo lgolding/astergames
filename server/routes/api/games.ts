2739037a435b92c3b5c373ad7f8c9a280dfeeed4
Larry Golding <larrygolding@comcast.net>
2021-02-26 14:51:03 -0800

    Use async/await in GET api/games.

--- server/routes/api/games.ts
+++ server/routes/api/games.ts
@@ -6,10 +6,13 @@ const router = express.Router();
 // @route GET api/games
 // @desc Gets all games
 // @access Public
-router.get('/', (_req, res) => {
-  Game.find()
-    .sort({ date: -1 })
-    .then(games => res.json(games));
+router.get('/', async (_req, res) => {
+  try {
+    let games = await Game.find().sort({ date: -1 });
+    res.json(games);
+  } catch (err) {
+    console.log(`Failed to GET games: ${err}`);
+  }
 });
 
 export default router;
