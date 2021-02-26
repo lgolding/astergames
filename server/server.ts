84299b6cc21dcdd441f8399abf72d705535aed49
Larry Golding <larrygolding@comcast.net>
2021-02-26 11:08:52 -0800

    Install mongodb+mongoose; connect to db.

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,22 @@
-console.log('Hello, server!');
+import express from 'express';
+import mongoose from 'mongoose';
+import { connectionString } from './config/keys';
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to database.'))
+  .catch(err => {
+    console.log(`Failed to connect to database: ${err}`);
+    process.exit(1);
+  });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}.`));
