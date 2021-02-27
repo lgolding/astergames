14047ff3fc0c687784fa8816d537ddf0e6ca2f5c
Larry Golding <larrygolding@comcast.net>
2021-02-27 15:50:40 -0800

    Introduce grid layout; draw crude board.

---
+++ client/src/components/BackgammonBoard.js
@@ -0,0 +1,39 @@
+import './backgammon.css';
+
+const BackgammonBoard = () => {
+  return (
+    <div className='board'>
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+
+      <div className='bar'></div>
+
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+      <div className='point point-light'></div>
+      <div className='point point-dark'></div>
+    </div>
+  )
+}
+
+export default BackgammonBoard
