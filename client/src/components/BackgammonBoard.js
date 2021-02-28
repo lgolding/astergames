e1fb11c9b1a02d31f2aeee68cecab82e7dd81c2f
Larry Golding <larrygolding@comcast.net>
2021-02-27 21:59:46 -0800

    Use array/map to generate points.

--- client/src/components/BackgammonBoard.js
+++ client/src/components/BackgammonBoard.js
@@ -6,38 +6,19 @@ import Game from '../games/Game';
 
 const BackgammonBoard = () => {
   let [ game, setGame ] = useState(new Game());
+  let indices = Array.from(Array(24).keys());
 
   return (
     <div className='board'>
-      <Point index={0} point={game.points[0]} />
-      <Point index={1} point={game.points[1]} />
-      <Point index={2} point={game.points[2]} />
-      <Point index={3} point={game.points[3]} />
-      <Point index={4} point={game.points[4]} />
-      <Point index={5} point={game.points[5]} />
+      {indices.slice(0, 6).map(index => (
+        <Point index={index} point={game.points[index]} />
+      ))}
 
       <Bar />
 
-      <Point index={6} point={game.points[6]} />
-      <Point index={7} point={game.points[7]} />
-      <Point index={8} point={game.points[8]} />
-      <Point index={9} point={game.points[9]} />
-      <Point index={10} point={game.points[10]} />
-      <Point index={11} point={game.points[11]} />
-
-      <Point index={12} point={game.points[12]} />
-      <Point index={13} point={game.points[13]} />
-      <Point index={14} point={game.points[14]} />
-      <Point index={15} point={game.points[15]} />
-      <Point index={16} point={game.points[16]} />
-      <Point index={17} point={game.points[17]} />
-
-      <Point index={18} point={game.points[18]} />
-      <Point index={19} point={game.points[19]} />
-      <Point index={20} point={game.points[20]} />
-      <Point index={21} point={game.points[21]} />
-      <Point index={22} point={game.points[22]} />
-      <Point index={23} point={game.points[23]} />
+      {indices.slice(6, 24).map(index => (
+        <Point index={index} point={game.points[index]} />
+      ))}
     </div>
   )
 }
--- client/src/components/Point.js
+++ client/src/components/Point.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
+const LIGHT_POINT_CLASS_NAME = 'point-light'
+const DARK_POINT_CLASS_NAME = 'point-dark';
+
 const Point = ({index, point}) => {
   let className;
   if (index < 12) {
-    className = index % 2 === 0 ? 'point-dark' : 'point-light';
+    className = index % 2 === 0 ? DARK_POINT_CLASS_NAME : LIGHT_POINT_CLASS_NAME;
   } else {
-    className = index % 2 === 0 ? 'point-light' : 'point-dark';
+    className = index % 2 === 0 ? LIGHT_POINT_CLASS_NAME : DARK_POINT_CLASS_NAME;
   }
 
   return (
     <div className={className}>
       {`${point.numLight}L`}
       <br/>
-      {`${point.numDark}L`}
+      {`${point.numDark}D`}
     </div>
   )
 }
