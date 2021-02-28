356713be377b46507bd134d4337e634c7d5c0269
Larry Golding <larrygolding@comcast.net>
2021-02-28 10:41:21 -0800

    Introduce consts for backgammon constants.

--- client/src/components/BackgammonBoard.js
+++ client/src/components/BackgammonBoard.js
@@ -3,23 +3,24 @@ import { useState } from 'react';
 import Point from './Point';
 import Bar from './Bar';
 import Game from '../games/Game';
+import { POINTS_ON_BOARD, POINTS_ON_TABLE } from '../games/constants';
 
 const BackgammonBoard = () => {
   let [game, setGame] = useState(new Game());
-  let indices = Array.from(Array(24).keys());
+  let indices = Array.from(Array(POINTS_ON_BOARD).keys());
 
   // We render the first 6 points (the top left table). The bar spans two rows
   // in the grid, so after rendering the bar, we can render the remaining 18
   // points without interruption.
   return (
     <div className='board'>
-      {indices.slice(0, 6).map(index => (
+      {indices.slice(0, POINTS_ON_TABLE).map(index => (
         <Point index={index} point={game.points[index]} />
       ))}
 
       <Bar />
 
-      {indices.slice(6, 24).map(index => (
+      {indices.slice(POINTS_ON_TABLE, POINTS_ON_BOARD).map(index => (
         <Point index={index} point={game.points[index]} />
       ))}
     </div>
---
+++ client/src/games/constants.js
@@ -0,0 +1,2 @@
+export const POINTS_ON_BOARD = 24;
+export const POINTS_ON_TABLE = POINTS_ON_BOARD / 4;
