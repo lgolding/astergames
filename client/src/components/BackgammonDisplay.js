5dc33bd3e0519950100f5565cac0b456b09da869
Larry Golding <larrygolding@comcast.net>
2021-03-09 17:48:47 -0800

    Play a dummy move on click.

--- client/src/components/BackgammonDisplay.js
+++ client/src/components/BackgammonDisplay.js
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import BackgammonBoard from './BackgammonBoard';
 import Game from '../games/Game';
+import Point from '../games/Point';
 
 const BackgammonDisplay = () => {
-  let [game, setGame] = useState(new Game());
-  let handleClick = () => alert('click!');
+  const [game, setGame] = useState(new Game());
+  const handleClick = () => {
+    const newGame = { ...game };
+    newGame.points[0] = new Point(3, 0);
+    newGame.points[1] = new Point(2, 0);
+    setGame(newGame);
+  };
 
   return (
     <>
