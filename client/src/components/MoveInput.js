f73c06c84f1ad43bc8b5b44f310cd653666afab6
Larry Golding <larrygolding@comcast.net>
2021-03-11 18:51:44 -0800

    Refocus after move.

--- client/src/components/MoveInput.js
+++ client/src/components/MoveInput.js
@@ -1,17 +1,24 @@
+import { React, useRef } from 'react';
+
 const MoveInput = ({ onMove }) => {
+  const fromField = useRef();
+  const toField = useRef();
+
   const submitHandler = event => {
     event.preventDefault();
-    const from = document.getElementById('fromPoint').value;
-    const to = document.getElementById('toPoint').value;
+    const from = fromField.current.value;
+    const to = toField.current.value;
     onMove(from, to);
+    fromField.current.focus();
+    fromField.current.select();
   };
 
   return (
     <form onSubmit={submitHandler}>
-      <label htmlFor='fromPoint'>From</label>
-      <input id='fromPoint' type='number' required />
-      <label htmlFor='fromPoint'>To</label>
-      <input id='toPoint' type='number' required />
+      <label htmlFor='fromField'>From</label>
+      <input ref={fromField} id='fromField' type='number' required />
+      <label htmlFor='toField'>To</label>
+      <input ref={toField} id='toField' type='number' required />
       <button>Move</button>
     </form>
   );
