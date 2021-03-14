b0bae7e95008d8b3bf5cefc0690866b5feadd70b
Larry Golding <larrygolding@comcast.net>
2021-03-14 11:36:29 -0700

    Improve MoveInput form appearance.

--- client/src/components/MoveInput.js
+++ client/src/components/MoveInput.js
@@ -1,4 +1,9 @@
 import { React, useRef } from 'react';
+import {
+  MOVE_INPUT_FORM_CLASS_NAME,
+  MOVE_INPUT_FORM_LABEL_CLASS_NAME,
+  MOVE_INPUT_FORM_BUTTON_CLASS_NAME,
+} from './classNames';
 
 const MoveInput = ({ onMove }) => {
   const fromField = useRef();
@@ -14,12 +19,16 @@ const MoveInput = ({ onMove }) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor='fromField'>From</label>
+    <form className={MOVE_INPUT_FORM_CLASS_NAME} onSubmit={submitHandler}>
+      <label className={MOVE_INPUT_FORM_LABEL_CLASS_NAME} htmlFor='fromField'>
+        From
+      </label>
       <input ref={fromField} id='fromField' type='number' required />
-      <label htmlFor='toField'>To</label>
+      <label className={MOVE_INPUT_FORM_LABEL_CLASS_NAME} htmlFor='toField'>
+        To
+      </label>
       <input ref={toField} id='toField' type='number' required />
-      <button>Move</button>
+      <button className={MOVE_INPUT_FORM_BUTTON_CLASS_NAME}>Move</button>
     </form>
   );
 };
--- client/src/components/classNames.js
+++ client/src/components/classNames.js
@@ -1,3 +1,7 @@
+export const MOVE_INPUT_FORM_CLASS_NAME = 'move-input-form';
+export const MOVE_INPUT_FORM_LABEL_CLASS_NAME = 'move-input-form__label';
+export const MOVE_INPUT_FORM_BUTTON_CLASS_NAME = 'move-input-form__button';
+
 export const POINT_NUMBER_CLASS_NAME = 'point-number';
 
 export const LIGHT_CHECKER_CLASS_NAME = 'checker-light';
