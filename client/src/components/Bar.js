5de4e66d45a7f2a1f24a64feac32acb1fe9c0245
Larry Golding <larrygolding@comcast.net>
2021-03-14 12:17:51 -0700

    Render checkers on bar.

--- client/src/components/Bar.js
+++ client/src/components/Bar.js
@@ -1,9 +1,18 @@
+import _ from 'lodash';
+import Checker from './Checker';
+import CheckerColor from '../games/CheckerColor';
 import './backgammon.css';
 
 const Bar = ({ player1, player2 }) => {
+  // TODO: Don't assume that player1 has the light checkers.
   return (
     <div className='bar'>
-      <span>{`${player1} / ${player2}`}</span>
+      {_.range(player1).map(() => (
+        <Checker color={CheckerColor.LIGHT} />
+      ))}
+      {_.range(player2).map(() => (
+        <Checker color={CheckerColor.DARK} />
+      ))}{' '}
     </div>
   );
 };
