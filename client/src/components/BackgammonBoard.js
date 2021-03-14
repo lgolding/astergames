6bbb06a00696c75c4a4df4bfe5f11d5707539862
Larry Golding <larrygolding@comcast.net>
2021-03-14 10:35:23 -0700

    Center point numbers in their cells

--- client/src/components/BackgammonBoard.js
+++ client/src/components/BackgammonBoard.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import './backgammon.css';
 import Point from './Point';
 import Bar from './Bar';
+import { POINT_NUMBER_CLASS_NAME } from './classNames';
 import { PLAYER1, PLAYER2, POINTS_ON_BOARD } from '../games/constants';
 
 const BackgammonBoard = ({ game }) => {
@@ -10,11 +11,15 @@ const BackgammonBoard = ({ game }) => {
   return (
     <div className='board'>
       {indices.slice(0, POINTS_ON_BOARD / 4).map(index => (
-        <div>{game.pointIndexToPointNumber(index)}</div>
+        <div className={POINT_NUMBER_CLASS_NAME}>
+          {game.pointIndexToPointNumber(index)}
+        </div>
       ))}
       <div></div>
       {indices.slice(POINTS_ON_BOARD / 4, POINTS_ON_BOARD / 2).map(index => (
-        <div>{game.pointIndexToPointNumber(index)}</div>
+        <div className={POINT_NUMBER_CLASS_NAME}>
+          {game.pointIndexToPointNumber(index)}
+        </div>
       ))}
       {indices.map(pointIndex => (
         <Point
@@ -26,11 +31,15 @@ const BackgammonBoard = ({ game }) => {
       {indices
         .slice(POINTS_ON_BOARD / 2, (3 * POINTS_ON_BOARD) / 4)
         .map(index => (
-          <div>{game.pointIndexToPointNumber(index)}</div>
+          <div className={POINT_NUMBER_CLASS_NAME}>
+            {game.pointIndexToPointNumber(index)}
+          </div>
         ))}
       <div></div>
       {indices.slice((3 * POINTS_ON_BOARD) / 4, POINTS_ON_BOARD).map(index => (
-        <div>{game.pointIndexToPointNumber(index)}</div>
+        <div className={POINT_NUMBER_CLASS_NAME}>
+          {game.pointIndexToPointNumber(index)}
+        </div>
       ))}
       <Bar player1={game.bar[PLAYER1]} player2={game.bar[PLAYER2]} />
     </div>
--- client/src/components/classNames.js
+++ client/src/components/classNames.js
@@ -1,2 +1,4 @@
+export const POINT_NUMBER_CLASS_NAME = 'point-number';
+
 export const LIGHT_CHECKER_CLASS_NAME = 'checker-light';
 export const DARK_CHECKER_CLASS_NAME = 'checker-dark';
