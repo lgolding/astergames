30a4d598ce921e38f90560cd6dd040fffcf0206d
Larry Golding <larrygolding@comcast.net>
2021-03-20 16:48:04 -0700

    Display pips on dice.

--- client/src/components/Die.js
+++ client/src/components/Die.js
@@ -1,33 +1,85 @@
 const Die = ({ face }) => {
-  const href = `#face${face}`;
-  console.log('href = ' + href);
+  const PIP_RADIUS = 10;
+  const VIEW_BOX_WIDTH = 100;
+  const VIEW_BOX_HEIGHT = 100;
+  const CENTER_X = VIEW_BOX_WIDTH / 2;
+  const CENTER_Y = VIEW_BOX_HEIGHT / 2;
+  const LEFT_X = VIEW_BOX_WIDTH / 5;
+  const RIGHT_X = (4 * VIEW_BOX_WIDTH) / 5;
+  const TOP_Y = VIEW_BOX_HEIGHT / 5;
+  const BOTTOM_Y = (4 * VIEW_BOX_HEIGHT) / 5;
+
   return (
     <div className='die'>
       <svg
-        position='absolute'
         width='100%'
         height='100%'
-        viewBox='0 0 100 100'
-        preserveAspectRatio='none'
+        viewBox={`0 0 ${VIEW_BOX_WIDTH} ${VIEW_BOX_HEIGHT}`}
       >
-        <use href={href} />
-        <symbol id='face1' width='100' height='100' viewBox='0 0 100 100'>
-          <polygon points='0,0 50,85 100,0' fill='red' />
+        <use href={`#face${face}`} />
+
+        <symbol
+          id='face1'
+          width='100'
+          height='100'
+          viewBox={`0 0 ${VIEW_BOX_WIDTH} ${VIEW_BOX_HEIGHT}`}
+        >
+          <circle cx={CENTER_X} cy={CENTER_Y} r={PIP_RADIUS} />
         </symbol>
-        <symbol id='face2' width='100' height='100' viewBox='0 0 100 100'>
-          <polygon points='0,0 50,85 100,0' fill='orange' />
+        <symbol
+          id='face2'
+          width='100'
+          height='100'
+          viewBox={`0 0 ${VIEW_BOX_WIDTH} ${VIEW_BOX_HEIGHT}`}
+        >
+          <circle cx={LEFT_X} cy={TOP_Y} r={PIP_RADIUS} />
+          <circle cx={RIGHT_X} cy={BOTTOM_Y} r={PIP_RADIUS} />
         </symbol>
-        <symbol id='face3' width='100' height='100' viewBox='0 0 100 100'>
-          <polygon points='0,0 50,85 100,0' fill='yellow' />
+        <symbol
+          id='face3'
+          width='100'
+          height='100'
+          viewBox={`0 0 ${VIEW_BOX_WIDTH} ${VIEW_BOX_HEIGHT}`}
+        >
+          <circle cx={LEFT_X} cy={TOP_Y} r={PIP_RADIUS} />
+          <circle cx={CENTER_X} cy={CENTER_Y} r={PIP_RADIUS} />
+          <circle cx={RIGHT_X} cy={BOTTOM_Y} r={PIP_RADIUS} />
         </symbol>
-        <symbol id='face4' width='100' height='100' viewBox='0 0 100 100'>
-          <polygon points='0,0 50,85 100,0' fill='green' />
+        <symbol
+          id='face4'
+          width='100'
+          height='100'
+          viewBox={`0 0 ${VIEW_BOX_WIDTH} ${VIEW_BOX_HEIGHT}`}
+        >
+          <circle cx={LEFT_X} cy={TOP_Y} r={PIP_RADIUS} />
+          <circle cx={RIGHT_X} cy={TOP_Y} r={PIP_RADIUS} />
+          <circle cx={LEFT_X} cy={BOTTOM_Y} r={PIP_RADIUS} />
+          <circle cx={RIGHT_X} cy={BOTTOM_Y} r={PIP_RADIUS} />
         </symbol>
-        <symbol id='face5' width='100' height='100' viewBox='0 0 100 100'>
-          <polygon points='0,0 50,85 100,0' fill='blue' />
+        <symbol
+          id='face5'
+          width='100'
+          height='100'
+          viewBox={`0 0 ${VIEW_BOX_WIDTH} ${VIEW_BOX_HEIGHT}`}
+        >
+          <circle cx={LEFT_X} cy={TOP_Y} r={PIP_RADIUS} />
+          <circle cx={RIGHT_X} cy={TOP_Y} r={PIP_RADIUS} />
+          <circle cx={CENTER_X} cy={CENTER_Y} r={PIP_RADIUS} />
+          <circle cx={LEFT_X} cy={BOTTOM_Y} r={PIP_RADIUS} />
+          <circle cx={RIGHT_X} cy={BOTTOM_Y} r={PIP_RADIUS} />
         </symbol>
-        <symbol id='face6' width='100' height='100' viewBox='0 0 100 100'>
-          <polygon points='0,0 50,85 100,0' fill='purple' />
+        <symbol
+          id='face6'
+          width='100'
+          height='100'
+          viewBox={`0 0 ${VIEW_BOX_WIDTH} ${VIEW_BOX_HEIGHT}`}
+        >
+          <circle cx={LEFT_X} cy={TOP_Y} r={PIP_RADIUS} />
+          <circle cx={CENTER_X} cy={TOP_Y} r={PIP_RADIUS} />
+          <circle cx={RIGHT_X} cy={TOP_Y} r={PIP_RADIUS} />
+          <circle cx={LEFT_X} cy={BOTTOM_Y} r={PIP_RADIUS} />
+          <circle cx={CENTER_X} cy={BOTTOM_Y} r={PIP_RADIUS} />
+          <circle cx={RIGHT_X} cy={BOTTOM_Y} r={PIP_RADIUS} />
         </symbol>
       </svg>
     </div>
